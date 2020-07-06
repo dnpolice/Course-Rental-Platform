c0b2306f61c408ad8ac9afbb4835388506043bd5
dnpolice <dylanpolicelli@gmail.com>
2020-07-05 22:09:26 -0400

    production db env variable added

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', {title: 'My Express App' , message: 'Hello'});
+    res.render('index', {title: 'Course Rentals' , message: 'Course Rental Platform'});
 });
 
 module.exports = router;
